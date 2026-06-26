5eb74207e96ebe57152a1d7201740f217e1f590f
Snehal Dattatray Kumbhar <snehsujata2305@gmail.com>
2026-06-26 22:26:04 +0530

    Create script.js

---
+++ Task14/script.js
@@ -0,0 +1,49 @@
+import * as utility from './utility.js'
+
+let n1 = 123;
+let n2 = 999;
+let n3 = 345;
+let n4 = 886;
+let n5 = 769;
+
+console.log("The number is:- "+n1);
+console.log(utility.sumOfN(n1));
+utility.table(n1);
+console.log(utility.isPrime(n1));
+utility.factors(n1);
+console.log(utility.sumOfDigits(n1));
+console.log(utility.isArmstrong(n1));
+
+
+console.log("The number is:- "+n2);
+console.log(utility.sumOfN(n2));
+utility.table(n2);
+console.log(utility.isPrime(n2));
+utility.factors(n2);
+console.log(utility.sumOfDigits(n2));
+console.log(utility.isArmstrong(n2));
+
+console.log("The number is:- "+n3);
+console.log(utility.sumOfN(n3));
+utility.table(n3);
+console.log(utility.isPrime(n3));
+utility.factors(n3);
+console.log(utility.sumOfDigits(n3));
+console.log(utility.isArmstrong(n3));
+
+console.log("The number is:- "+n4);
+console.log(utility.sumOfN(n4));
+utility.table(n4);
+console.log(utility.isPrime(n4));
+utility.factors(n4);
+console.log(utility.sumOfDigits(n4));
+console.log(utility.isArmstrong(n4));
+
+console.log("The number is:- "+n5);
+console.log(utility.sumOfN(n5));
+utility.table(n5);
+console.log(utility.isPrime(n5));
+utility.factors(n5);
+console.log(utility.sumOfDigits(n5));
+console.log(utility.isArmstrong(n5));
+

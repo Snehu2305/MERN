9d8c4effaae90d1bb047bef6c316df36835163be
Snehal Dattatray Kumbhar <snehsujata2305@gmail.com>
2026-08-03 22:35:44 +0530

    Create Shoes.js

---
+++ Task27/data/Shoes.js
@@ -0,0 +1,52 @@
+const Shoes = [
+  {
+    id: 1,
+    name: "White Casual Sneaker",
+    price: 70,
+    image: "https://tse1.mm.bing.net/th/id/OIP.60wPWfFu2FDKBDUIwcIEPAHaE7?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+  },
+  {
+    id: 2,
+    name: "Blue and White Nike Shoe",
+    price: 100,
+    image: "https://images.pexels.com/photos/13236693/pexels-photo-13236693.jpeg?cs=srgb&dl=pexels-rafael-quaty-13236693.jpg&fm=jpg"
+  },
+  {
+    id:3,
+    name: "Athletic Sneaker",
+    price: 60,
+    image: "https://tse3.mm.bing.net/th/id/OIP.i7i5Uxh-ONNkHgxA_pu8QgHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+  },
+  {
+    id: 4,
+    name: "kids Sneaker",
+    price: 40,
+    image: "https://i5.walmartimages.com/seo/Sopiago-Kid-Shoes-Girl-Kids-Breathable-Mesh-Running-Shoes-Casual-Athletic-Sneakers-for-Girls-Pink-Little-kid-Big-kid-2-5_738c9022-cdb9-4d6e-89bc-79352d723e50.52023857cbffd8b0d83ecd044b2d6365.jpeg"
+  },
+  {
+    id: 5,
+    name: "women's Sneaker",
+    price: 80,
+    image: "https://i5.walmartimages.com/seo/Kesitin-Women-s-Mesh-Breathable-Athletic-Walking-Shoes-Lace-up-Running-Sneakers-Gym-Outdoor-Shoes_ba7ce1e5-8735-47bd-8e14-d9e0982f6dd8.8f98cb5d54b064d189d5f12cc41a10ae.jpeg"
+  },
+  {
+    id: 6,
+    name: "Korean girl's Sneaker",
+    price: 150,
+    image: "https://down-my.img.susercontent.com/file/id-11134201-23020-jclq41evbunvdf"
+  },
+  {
+    id: 7,
+    name: "Running Shoes",
+    price: 90,
+    image: "https://i.pinimg.com/736x/ee/ac/5f/eeac5f6221e4bc4ca057263132ff5b88.jpg"
+  },
+  {
+    id: 8,
+    name: "Aqua Ripple Sneaker",
+    price: 120,
+    image: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1720112355_6238943.jpg?v=2"
+  },
+];
+
+export default Shoes

379c8306ab54cedbfdabb719223e997266d8bd25
Snehal Dattatray Kumbhar <snehsujata2305@gmail.com>
2026-08-08 19:16:35 +0530

    Add files via upload

---
+++ Task30/Components/index.js
@@ -0,0 +1,8 @@
+import Home from "./home";
+import Contact from "./Contact";
+import About from "./About";
+import Artwork from "./Artwork";
+import Gallery from "./Gallery";
+import Layout from "./Layout";
+
+export {Home, Contact, About, Artwork, Gallery, Layout}

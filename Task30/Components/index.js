e57afb80593a4822aba203bb1723fc931addfcbd
Snehal Dattatray Kumbhar <snehsujata2305@gmail.com>
2026-08-15 19:16:25 +0530

    Delete Task30 directory

--- Task30/Components/index.js
+++
@@ -1,8 +0,0 @@
-import Home from "./home";
-import Contact from "./Contact";
-import About from "./About";
-import Artwork from "./Artwork";
-import Gallery from "./Gallery";
-import Layout from "./Layout";
-
-export {Home, Contact, About, Artwork, Gallery, Layout}
